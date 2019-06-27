fb0f8b6b84ee8d195dbb8fd87f1dc3b903481fc9
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-06-27 20:01:19 +0530

    Read file successfully

--- first.js
+++ first.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+var initialize=function(){
+const xlsx = require('xlsx');
+var wb =xlsx.readFile('./rawData.xlsx');
+var ws=wb.Sheets[wb.SheetNames[0]];
+    data =xlsx.utils.sheet_to_json(ws);
+    data.forEach(element => {
+    if (element.vol !=undefined)
+        {
+            console.log(element.SKU)
+            totalBoxVol=totalBoxVol+element.Vol;
+        }
+        else
+        {
+            console.log(1+element.vol)
+        data.splice(element);
+        }
+    });
+};
+var totalBoxVol=0.0,
+    totalCrateVol=0.0,
+    data;
+
+initialize();
+
+console.log(data);
