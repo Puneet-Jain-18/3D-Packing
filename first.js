0fdb6acc0666890e64453e8da31d2c5dca5a2197
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-06-28 22:48:35 +0530

    corrected mistskes

--- first.js
+++ first.js
@@ -4,22 +4,20 @@ var wb =xlsx.readFile('./rawData.xlsx');
 var ws=wb.Sheets[wb.SheetNames[0]];
     data =xlsx.utils.sheet_to_json(ws);
     data.forEach(element => {
-    if (element.vol !=undefined)
+    if (element.vol)
         {
+            data_final.push(element);
             console.log(element.SKU)
-            totalBoxVol=totalBoxVol+element.Vol;
-        }
-        else
-        {
-            console.log(1+element.vol)
-        data.splice(element);
+            totalBoxVol=totalBoxVol+element.vol;
         }
     });
 };
 var totalBoxVol=0.0,
     totalCrateVol=0.0,
-    data;
+    data,
+    data_final=[];
 
 initialize();
 
-console.log(data);
+console.log(data_final);
+console.log(totalBoxVol)
