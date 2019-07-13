231f78d59d560c3171c29444f85c5b5fe12c94cd
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-07-13 17:53:06 +0530

    completed for one pallet

--- main.js
+++ main.js
@@ -85,121 +85,129 @@ var layer= function(){
         yHighest=0,
         palArea=xEnd*zEnd,
         unpacked=[],
-        f=0;
+        vol=0,
+        flag=0;
+        pFlag=1;
 
-while(f<=6)
-{
-f+=1;
-
-
-    currBoxList.forEach((element,index)=>{
-        var or1=0,or2=0,or3=0;
-        if(element.width<=yEnd)
-         or1=Math.floor(palArea/(element.length*element.height));
-
-        if(element.height<=yEnd)
-         or2=Math.floor(palArea/(element.length*element.width));
-
-         if(element.length<=yEnd)
-         or3=Math.floor(palArea/(element.width*element.height));
-
-        var best=Math.max(or1,or2,or3);
-        if(best>0)
+    while(pFlag)
+    {
+        pFlag=0;
+        if(flag)
         {
-            var packx,packy;
-            var final
-            if(best==or1)
-                {
-                    final=or1;
-                    packx=element.length;
-                    packy=element.width;
-                    packz=element.height;
-                }
-            else if(best==or2)
-             {
-                final=or2;  
-                packx=element.length;
-                packy=element.height;
-                packz=element.width;
-                
-             } 
-            else
-                {
-                    final=or3;
-                    packx=element.width;
-                    packy=element.length;
-                    packz=element.height;
-                }
-               
-        ///////////////////////////
-            if(final >= element.quantity && (zEnd-zStart)>=packz && yEnd>=packy)
-            {
-                let n=element.quantity;
-                console.log("SKU: "+element.SKU+" Place all "+element.quantity+" packets from x= "+xStart+" z= "+zStart );
-                
-                xStart+=Math.ceil(n*packx);
-
-                zHighest=Math.max(zHighest,packz)
-                yHighest=Math.max(yHighest,packy)
-
-                if(xEnd-xStart<1)
-                {
-                    xStart=0;
-                    zStart+=zHighest;
-                    zHighest=0;
-                }
-                quantity+=1;
-                palArea=((xEnd-xStart)*(zEnd-zStart))
-                console.log("New xStart= "+xStart+" New z-start= "+zStart+" Area= "+palArea);
-                console.log(".");
-            }
-            else if(element.quantity/final>=1 && (zEnd-zStart)>=packz && yEnd>=packy) 
-            {
-                let n=Math.floor(element.quantity/final);
-                console.log("SKU: "+element.SKU+" Place ONLY "+n+" packets from x= "+xStart+" z= "+zStart );
-                
-                xStart+=Math.ceil(n*packx);
-
-                zHighest=Math.max(zHighest,packz)
-                yHighest=Math.max(yHighest,packy)
-
-                if(xEnd-xStart<1)
-                {
-                    xStart=0;
-                    zStart+=zHighest;
-                    zHighest=0;
-                }
-                palArea=((xEnd-xStart)*(zEnd-zStart))
-                console.log("New xStart= "+xStart+" New z-start= "+zStart+" Area= "+palArea);
-                console.log(".");
-                element.quantity-=n;
-                unpacked.push(element);
-            }
-            else
-            {
-                unpacked.push(element);
-            }
-
+            currBoxList=unpacked;
+            unpacked=[];
         }
-        else
-        unpacked.push(element);
-    })
-    currBoxList=unpacked;
-    unpacked=[];
-  // yEnd-=yHighest;
-    zStart=0;
-    zHighest=0;
-    xStart=0;
-    yHighest=0;
-}
+        flag+=1;
+        currBoxList.forEach((element,index)=>{
+            var or1=0,or2=0,or3=0;
+            if(element.width<=yEnd)
+            or1=Math.floor(palArea/(element.length*element.height));
+
+            if(element.height<=yEnd)
+            or2=Math.floor(palArea/(element.length*element.width));
+
+            if(element.length<=yEnd)
+            or3=Math.floor(palArea/(element.width*element.height));
+
+            var best=Math.max(or1,or2,or3);
+            if(best>0)
+            {
+                var packx,packy;
+                var final
+                if(best==or1)
+                    {
+                        final=or1;
+                        packx=element.length;
+                        packy=element.width;
+                        packz=element.height;
+                    }
+                else if(best==or2)
+                {
+                    final=or2;  
+                    packx=element.length;
+                    packy=element.height;
+                    packz=element.width;
+                    
+                } 
+                else
+                    {
+                        final=or3;
+                        packx=element.width;
+                        packy=element.length;
+                        packz=element.height;
+                    }
+                
+            ///////////////////////////
+                if(final >= element.quantity && (zEnd-zStart)>=packz && yEnd>=packy)
+                {
+                    let n=element.quantity;
+                    vol+=(n*(packx*packy));
+                    console.log("SKU: "+element.SKU+" Place all "+element.quantity+" packets from x= "+xStart+" z= "+zStart );
+                    
+                    xStart+=Math.ceil(n*packx);
+
+                    zHighest=Math.max(zHighest,packz)
+                    yHighest=Math.max(yHighest,packy)
+
+                    if(xEnd-xStart<1)
+                    {
+                        xStart=0;
+                        zStart+=zHighest;
+                        zHighest=0;
+                    }
+                    quantity+=1;
+                    palArea=((xEnd-xStart)*(zEnd-zStart))
+                    console.log("New xStart= "+xStart+" New z-start= "+zStart+" Area= "+palArea);
+                    console.log(".");
+                    pFlag=1;
+                }
+                else if((zEnd-zStart)>=packz && yEnd>=packy) 
+                {
+                    let n=final;
+                    console.log("SKU: "+element.SKU+" Place ONLY "+n+" packets from x= "+xStart+" z= "+zStart );
+                    
+                    xStart+=Math.ceil(n*packx);
+                    vol+=(n*(packx*packy));
+
+                    zHighest=Math.max(zHighest,packz)
+                    yHighest=Math.max(yHighest,packy)
+
+                    if(xEnd-xStart<1)
+                    {
+                        xStart=0;
+                        zStart+=zHighest;
+                        zHighest=0;
+                    }
+                    palArea=((xEnd-xStart)*(zEnd-zStart))
+                    console.log("New xStart= "+xStart+" New z-start= "+zStart+" Area= "+palArea);
+                    console.log(".");
+                    element.quantity-=n;
+                    unpacked.push(element);
+                    pFlag=1;
+                }
+                else
+                {
+                    unpacked.push(element);
+                }
+
+            }
+            else
+            unpacked.push(element);
+        })
+    //yEnd-=yHighest;
+        zStart=0;
+        zHighest=0;
+        xStart=0;
+        yHighest=0;
+        palArea=(xEnd-xStart)*(zEnd-zStart);
+    }
     console.log("Remainig Gap x= ",(xEnd-xStart),(zEnd-zStart));
     console.log("Total quantity packed = ",quantity);
     console.log("Remaining to be packed = ", unpacked.length,currBoxList.length);
-    console.log(yEnd,xStart,zStart)
-    console.log(0,xEnd,zEnd)
+    console.log("Volume wasted = ",vol,yEnd)
     console.log(data.crate[crateIndex]);
+    console.log(unpacked)
 
-    console.log(currBoxList);
 }
 layer();
 
